149630dcca20f2a3dbbbfb9fa90263383c5192c1
Pathairush Seeda <p.patcoding@gmail.com>
2022-05-02 17:02:06 +0700

    feat: add sample data and output section

--- lead_app.py
+++ lead_app.py
@@ -7,40 +7,57 @@ from matplotlib.style import available
 import streamlit as st
 import pandas as pd
 import datetime as dt
+import pandasql as ps
+
+@st.cache
+def load_data():
+    data = pd.read_csv('fake_data.csv')
+    return data
+
+data = load_data()
+available_cols = data.columns
 
 def fix_template():
 
+    # header
     st.title('Lead Generation Application')
+    st.header('Sample data')
+    st.write(data.head(10))
+
+    # Conditions 
     st.header('Inputs')
 
-    # date
-    col1, col2 = st.columns(2)
-    st.session_state['input_start_date'] = col1.date_input("Transaction effective start date", value = dt.datetime.now().replace(month=1, day=1), on_change = fix_template if not st.session_state.num_filter else update_filters)
-    st.session_state['input_end_date'] = col2.date_input("Transaction effective end date", on_change = fix_template if not st.session_state.num_filter else update_filters)
+    # partitioned date condition
+    last_eom_date = dt.datetime.now().replace(day=1) - dt.timedelta(days=1)
+    st.session_state['input_as_of_date'] = st.date_input('As of date',  value = last_eom_date, on_change = fix_template if not st.session_state.num_filter else update_filters)
 
-    # pre-defines
-    st.session_state['input_products'] = st.multiselect("Product", ["Fund", "Stock", "Offshore"], default='Fund', on_change = fix_template if not st.session_state.num_filter else update_filters)
+    # mandatory conditions
+    input_products_choices = ["Fund", "Stock", "Offshore"]
+    st.session_state['input_products'] = st.multiselect("Product", input_products_choices, on_change = fix_template if not st.session_state.num_filter else update_filters)
     st.session_state['input_fund_categories'] = None
-    if "Fund" in st.session_state['input_products'] or "All" in st.session_state['input_products']:
-        st.session_state['input_fund_categories'] = st.multiselect('Fund Category', ["All", "Money market", "Equity", "Fixed Income"], default = "All", on_change = fix_template if not st.session_state.num_filter else update_filters)
+    if "Fund" in st.session_state['input_products']:
+        input_fund_category_choices = ["Money market", "Equity", "Fixed Income"]
+        st.session_state['input_fund_categories'] = st.multiselect('Fund Category', input_fund_category_choices, on_change = fix_template if not st.session_state.num_filter else update_filters)
 
+    # remember that the fix tempalte has been called.
     st.session_state.is_fix_template_called = True
 
 # dynamic conditions
 
 def dynamic_condition(i):
     '''
-    Return new dynamic filters
+    Return a new dynamic filter
     '''
-    available_cols = [f'col_{i}' for i in range(1,11)]
-    col1, col2, col3 = st.columns(3)
-    col1.selectbox(f'condition_{i}', ['AND', 'OR'], key = f'input_condition_{i}', on_change = update_filters)
-    col2.selectbox(f'feature_{i}', available_cols, key = f'input_feature_{i}', on_change = update_filters)
-    col3.number_input(f'value_{i}', 0, key = f'input_value_{i}', on_change = update_filters)
+    i += 1
+    col1, col2, col3, col4 = st.columns(4)
+    col1.selectbox(f'condition_{i}', ['AND', 'OR'], key = f'input{i}_condition', on_change = update_filters)
+    col2.selectbox(f'feature_{i}', available_cols, key = f'input{i}_feature', on_change = update_filters)
+    col3.selectbox(f'sign_{i}', ['>', '<', "=", '>=', '<=', 'IN'], key = f'input{i}_sign', on_change = update_filters)
+    col4.text_input(f'value_{i}', 0, key = f'input{i}_value', on_change = update_filters)
 
 def update_filters():
     '''
-    load the fix tempalte and current filters
+    Load fix_template() and populate current filters
     '''
 
     st.session_state.is_fix_template_called = False
@@ -50,20 +67,16 @@ def update_filters():
     for _ in range(st.session_state.num_filter):
         dynamic_condition(_)
 
-    # st.session_state.is_submit_button_rendered = False
-    # if not st.session_state.is_submit_button_rendered:
-    #     submit = st.button('submit')
-
 def add_filters():
     '''
-    load the fix tempalte and current filters plus new filters
+    Load fix_template() and populate a new filter
     '''
     
     st.session_state.is_fix_template_called = False
     if not st.session_state.is_fix_template_called:
         fix_template()
 
-    st.session_state.num_filter += 1
+    st.session_state.num_filter += 1 # increase the number of filter
     for _ in range(st.session_state.num_filter):
         dynamic_condition(_)
 
@@ -73,17 +86,16 @@ def remove_filter():
     if not st.session_state.is_fix_template_called:
         fix_template()
 
-    st.session_state.num_filter -= 1
+    st.session_state.num_filter -= 1 # reduce the number of filter
     for _ in range(st.session_state.num_filter):
         dynamic_condition(_)
 
-    # delte state
-    current_state = st.session_state.num_filter
-    for key in ['condition', 'feature', 'value']:
-        del st.session_state[f'{key}_{current_state}']
+    # delete the latest filter fomr sessionstate 
+    delete_state_number = st.session_state.num_filter + 1
+    for key in ['condition', 'feature', 'value', 'sign']:
+        del st.session_state[f'input{delete_state_number}_{key}']
 
 # Inital state
-
 if 'num_filter' not in st.session_state:
     st.session_state.num_filter = 0
 
@@ -93,47 +105,50 @@ if 'is_fix_template_called' not in st.session_state:
 if not st.session_state.is_fix_template_called:
     fix_template()
 
+# Buttons
 col1, col2, col3 = st.columns(3)
 add_button = col1.button('add_filter', on_click = add_filters),
 refresh_button = col2.button('refresh', on_click = update_filters),
 remove_button = col3.button('remove_filter', on_click = remove_filter),
-submit_button = st.button("submit", on_click=update_filters)
+lead_button = st.button("Generate Lead!", on_click=update_filters)
 
-# submit
-filter_values = {}
-if submit_button:
+# Generate lead
+if lead_button:
 
+    # convert input params to dict
     input_args = dict(st.session_state)
-    sorted_dict = dict(sorted(input_args.items()))
-    st.write(sorted_dict)
+    input_dict = dict(sorted(input_args.items()))
+    # st.write(input_dict)
+
+    # process mandatory conditions
+    predefined_conds = {
+    'input_as_of_date' : f''' dt = '{input_dict['input_as_of_date'].strftime("%Y%m%d")}' ''',
+    'input_products' : f'''\n AND products IN {input_dict['input_products']}''',
+    'input_fund_categories' : f'''\n AND sub_products_fund IN {input_dict['input_fund_categories']}'''
+    }
     
-    
-    # group of conditions
-    # filter_values['dates'] = {'start_date' : start_date, 'end_date' : end_date}
-    # filter_values['products'] = {'products' : products, 'sub_products_fund' : fund_categories}
-    # filter_values['behavior'] = {'product_txn_amt' : product_txn_amt, 'product_aum_amt' : product_aum_amt}
-    # st.write(filter_values)
+    # process dynamic conditions
+    dynamic_conds = []
+    for i in range(1, input_dict['num_filter'] + 1):
+        dynamic_conds.append(
+            " ".join([input_dict[f'input{i}_condition'], input_dict[f'input{i}_feature'], input_dict[f'input{i}_sign'], input_dict[f'input{i}_value']])
+        )
 
-#     predefined_conds = {
-#     'dates' : f''' dt BETWEEN '{filter_values['dates']['start_date'].strftime("%Y%m%d")}' AND '{filter_values['dates']['end_date'].strftime("%Y%m%d")}' ''',
-#     'products' : f'''\n AND products IN {filter_values['products']['products']}''',
-#     'sub_product_fund' : f'''\n AND sub_products_fund IN {filter_values['products']['sub_products_fund']}''',
-#     'product_txn_amt' : f'''\n AND product_txn_amt >= {product_txn_amt}''',
-#     'product_aum_amt' : f'''\n AND product_aum_amt >= {product_aum_amt}''',
-#     }
+    # consoldiate conditions
+    where_conds = ""
+    for cond in input_dict:
+        if cond in predefined_conds and input_dict[cond]:
+            where_conds += predefined_conds[cond]
+    for cond in dynamic_conds:
+        where_conds += cond
 
-#     where_conds = ""
-#     for cond in filter_values:
-#         for element in filter_values[cond]:
-#             if element is not None:
-#                 where_conds += predefined_conds[cond]
+    query_tempalte = f'''
+    \n SELECT *
+    \n FROM data
+    \n WHERE {where_conds}
+    '''
+    # st.write(f"Output query : {query_tempalte}")
 
-#     query_tempalte = f'''
-#     SELECT *
-#     \n FROM table
-#     \n WHERE {where_conds}
-#     '''
-
-#     st.write(filter_values)
-#     st.write(f"Output query : {query_tempalte}")
-
+    # Output result
+    st.header('Output')
+    st.write(ps.sqldf(query_tempalte, locals()))
